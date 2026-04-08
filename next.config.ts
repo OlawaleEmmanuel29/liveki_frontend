f96028f7ee376c5586b02838aa9e61c6f9f8e41d
OlawaleEmmanuel29 <157608193+OlawaleEmmanuel29@users.noreply.github.com>
2026-04-08 18:38:02 +0100

    Add x-sandbox-id and Authorization to CORS headers

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,8 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" }, 
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          // Added x-sandbox-id and Authorization to the list below
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-sandbox-id, Authorization" },
         ]
       }
     ]
